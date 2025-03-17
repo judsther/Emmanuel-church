16570bc11a231f1771ecdb2e1aec81f8338a8f64
judsther <j.arevaloguardado@gmail.com>
2025-03-17 16:42:22 -0600

    trying email sender with formsubmit

--- src/pages/Ministerios/components/MailForm.jsx
+++ src/pages/Ministerios/components/MailForm.jsx
@@ -1,67 +1,88 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 export default function MailForm() {
-
-    const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({
     name: "",
     number: "",
     email: "",
     message: "",
-    });
+  });
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value});
-    };
-
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      try {
-          const response = await fetch("https://emmanuel-church-six.vercel.app/api/sendMail", {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify(formData),
-          });
-  
-          const text = await response.text();
-          let result;
-          try {
-              result = JSON.parse(text);
-          } catch (error) {
-              throw new Error("Respuesta inválida del servidor: " + text);
-          }
-  
-          alert(result.message);
-      } catch (error) {
-          console.error("Error en handleSubmit:", error);
-          alert("Hubo un error al enviar el mensaje. Inténtalo de nuevo.");
-      }
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   return (
     <div>
-          <form className="flex flex-col gap-4 items-end justify-center" onSubmit={handleSubmit}>
-          <div>
-           <label htmlFor="name" className="">Nombre: </label>
-           <input type="text" name="name" id="name" autoComplete="name" placeholder=" tu nombre" required onChange={handleChange} className=" w-80 h-10 border border-amber-400 bg-white rounded-md" />
-           </div>
-            <div>
-            <label htmlFor="email" className="">Email: </label>
-           <input type="email" autoComplete="email" name="email" id="email" placeholder=" tucorreo@ejemplo.com" required onChange={handleChange} className=" w-80 h-10 border border-amber-400 bg-white rounded-md" />
-           </div>
-           <div>
-           <label htmlFor="message" className="">Mensaje: </label>
-           <textarea name="message" id="message" placeholder=" tu mensaje" required onChange={handleChange} className=" w-80 h-18 border border-amber-400 bg-white rounded-md" />
-           </div>
-           <div>
-           <label htmlFor="number" className="">Número: </label>
-           <input type="text" name="number" id="number" autoComplete="number" placeholder=" tu número de WhatsApp (opcional)" onChange={handleChange} className=" w-80 h-10 border border-amber-400 bg-white rounded-md" />
-           </div>
-           <button type="submit" className="py-2 px-4 bg-white border border-amber-400 hover:bg-amber-200 shadow-md text-center text-zinc-800 rounded font-sans font-semibold">
-              Enviar
-           </button>
-          </form>
+      <form
+        className="flex flex-col gap-4 items-end justify-center"
+        action="https://formsubmit.co/mcasanvicente@gmail.com"
+        method="POST"
+      >
+        <div>
+          <label htmlFor="name" className="">
+            Nombre:{" "}
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            autoComplete="name"
+            placeholder=" tu nombre"
+            required
+            onChange={handleChange}
+            className=" w-80 h-10 border border-amber-400 bg-white rounded-md"
+          />
+        </div>
+        <div>
+          <label htmlFor="email" className="">
+            Email:{" "}
+          </label>
+          <input
+            type="email"
+            autoComplete="email"
+            name="email"
+            id="email"
+            placeholder=" tucorreo@ejemplo.com"
+            required
+            onChange={handleChange}
+            className=" w-80 h-10 border border-amber-400 bg-white rounded-md"
+          />
+        </div>
+        <div>
+          <label htmlFor="message" className="">
+            Mensaje:{" "}
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder=" tu mensaje"
+            required
+            onChange={handleChange}
+            className=" w-80 h-18 border border-amber-400 bg-white rounded-md"
+          />
+        </div>
+        <div>
+          <label htmlFor="number" className="">
+            Número:{" "}
+          </label>
+          <input
+            type="text"
+            name="number"
+            id="number"
+            autoComplete="number"
+            placeholder=" tu número de WhatsApp (opcional)"
+            onChange={handleChange}
+            className=" w-80 h-10 border border-amber-400 bg-white rounded-md"
+          />
+        </div>
+        <button
+          type="submit"
+          className="py-2 px-4 bg-white border border-amber-400 hover:bg-amber-200 shadow-md text-center text-zinc-800 rounded font-sans font-semibold"
+        >
+          Enviar
+        </button>
+      </form>
     </div>
-  )
+  );
 }
