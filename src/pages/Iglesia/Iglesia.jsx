91871bb5e0d9a526afd67cd00837ab6ef87c3546
judsther <j.arevaloguardado@gmail.com>
2025-04-02 19:11:26 -0600

    fixing dif production view

--- src/pages/Iglesia/Iglesia.jsx
+++ src/pages/Iglesia/Iglesia.jsx
@@ -86,20 +86,17 @@ export default function Iglesia() {
   </p>
 
   <div className="w-full overflow-x-auto scroll-smooth whitespace-nowrap">
-    <div className="inline-flex h-auto space-x-4"> 
+    <div className="inline-flex h-48 space-x-4"> 
       {images.map((img, index) => (
-       
         <div 
           key={index} 
-          className="inline-block flex-shrink-0" 
+          className="inline-block h-48 w-auto flex-shrink-0" 
         >
-           <div className="h-58 w-auto flex items-center justify-center">
           <img 
             src={img.src} 
             alt={img.alt || "Imagen de galería"} 
-            className="h-full w-full object-cover rounded"
+            className="h-full w-full object-cover rounded-lg"
           />
-          </div>
         </div>
       ))}
     </div>
